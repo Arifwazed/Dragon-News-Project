245c04c8d9587469e04957af2ff627d77890c2a7
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-10-19 00:55:18 +0600

    responsive

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -18,11 +18,11 @@ const HomeLayout = () => {
                     <Navbar></Navbar>
                 </nav>
             </header>
-            <main className='w-11/12 mx-auto my-5 *:border grid grid-cols-12 gap-4'>
-                <aside className='col-span-3 sticky top-0 h-fit'>
+            <main className='w-11/12 mx-auto my-5 *:border grid grid-cols-3 md:grid-cols-12 gap-4'>
+                <aside className='col-span-3 md:sticky top-0 h-fit'>
                     <LeftAside></LeftAside>
                 </aside>
-                <section className="main-side col-span-6">
+                <section className="main-side col-span-3 md:col-span-6">
                     <Outlet></Outlet>
                 </section>
                 <aside className='col-span-3 sticky top-0 h-fit'>
